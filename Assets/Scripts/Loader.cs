fe97e73ecc935e71ea4cd9f09fc0fee6702e0127
lebbis <44391814+lebbis@users.noreply.github.com>
2020-11-23 09:41:19 +0200

    first commit

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using System;
+using UnityEngine.Experimental.UIElements;
+
+public class Loader : MonoBehaviour
+{
+    public UnityEngine.UI.Button yourButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.UI.Button btn = yourButton.GetComponent<UnityEngine.UI.Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        
+
+    }
+
+    public void TaskOnClick()
+    {
+        Application.OpenURL(@"https://1drv.ms/b/s!AqCNKHWXN2e1bWVj6sDpkHb1eY8");
+
+      
+    }
+
+    
+
+
+    // Update is called once per frame
+    public void OnButton()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WebURLScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebURLScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void btnOne()
+    {
+        Application.OpenURL("https://youtu.be/2q5rl7Hsd1Q?t=20");
+    }
+    public void btnTwo()
+    {
+        Application.OpenURL("https://1drv.ms/b/s!AqCNKHWXN2e1bLrximF5diYx4hU");
+    }
+
+    public void btnThree()
+    {
+        Application.OpenURL("https://1drv.ms/b/s!AqCNKHWXN2e1bmxuLp8FShyc-Ck");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
